412c7a0aa04c7f7118efa4c4c2aefe39f6ec9c49
JoeScho <joeschofield@live.co.uk>
2018-09-10 15:10:16 +0200

    test: error scenarios

--- test/index.test.js
+++ test/index.test.js
@@ -4,24 +4,47 @@ const { expect } = require('chai');
 const { get } = require('../index');
 
 describe('get', () => {
-  it('should retrieve the parameter from the request body if present', done => {
-    const req = { body: { myParam: 'this is my param' } };
-    const param = get(req, 'myParam');
-    expect(param).to.equal('this is my param');
-    done();
+  describe('success', () => {
+    it('should retrieve the parameter from the request body if present', done => {
+      const req = { body: { myParam: 'this is my param' } };
+      const param = get(req, 'myParam');
+      expect(param).to.equal('this is my param');
+      done();
+    });
+
+    it('should retrieve the parameter from the request query if present', done => {
+      const req = { query: { myParam: 'this is my param' } };
+      const param = get(req, 'myParam');
+      expect(param).to.equal('this is my param');
+      done();
+    });
+
+    it('should retrieve the parameter from the request body if both body and query are present', done => {
+      const req = { query: { myParam: 'this is my query param' }, body: { myParam: 'this is my body param' } };
+      const param = get(req, 'myParam');
+      expect(param).to.equal('this is my body param');
+      done();
+    });
   });
 
-  it('should retrieve the parameter from the request query if present', done => {
-    const req = { query: { myParam: 'this is my param' } };
-    const param = get(req, 'myParam');
-    expect(param).to.equal('this is my param');
-    done();
-  });
+  describe('errors', () => {
+    it('should return false if req is null', done => {
+      const param = get(null, 'myParam');
+      expect(param).to.equal(false);
+      done();
+    });
 
-  it('should retrieve the parameter from the request body if both body and query are present', done => {
-    const req = { query: { myParam: 'this is my query param' }, body: { myParam: 'this is my body param' } };
-    const param = get(req, 'myParam');
-    expect(param).to.equal('this is my body param');
-    done();
+    it('should return false if req is undefined', done => {
+      const param = get(undefined, 'myParam');
+      expect(param).to.equal(false);
+      done();
+    });
+
+    it('should return false if the param is not present in the query or body', done => {
+      const req = { query: { someOtherParam: 'this is my query param' }, body: { someOtherParam: 'this is my body param' } };
+      const param = get(req, 'myParam');
+      expect(param).to.equal(false);
+      done();
+    });
   });
 });
